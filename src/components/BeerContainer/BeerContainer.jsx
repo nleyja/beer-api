d8afaad381f7d0fe6384c308e057c6a91fb92b0d
Noah <nleyja@gmail.com>
2022-08-17 17:49:54 -0500

    added some basic styling

--- src/components/BeerContainer/BeerContainer.jsx
+++ src/components/BeerContainer/BeerContainer.jsx
@@ -5,9 +5,15 @@ import BeerCards from "../BeerCards/BeerCards"
 const BeerContainer =  (props) => {
 
   const {beersArr} = props;
+  // const {name, img_url, tagline} = beers
+  //key value beersArr is now props
 
   const beerProfile = beersArr.map((beer) => {
-    return beer.name
+
+    const {name, image_url, tagline} = beer
+    return (
+      <BeerCards className="beer-card" image_url={image_url} name={name} tagline={tagline} />
+    )
   })
 
   return(
@@ -17,4 +23,4 @@ const BeerContainer =  (props) => {
   )
 }
 
-export default BeerContainer
+export default BeerContainer;
