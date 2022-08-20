ea311445ffe1693ad6f1f485cde7e1005be3d523
Noah <nleyja@gmail.com>
2022-08-19 20:47:51 -0500

    added search bar functionality

--- src/components/NavMenu/NavMenu.jsx
+++ src/components/NavMenu/NavMenu.jsx
@@ -7,6 +7,8 @@ const NavMenu = (props) => {
 
   const {beersArr, setBeers} = props;
 
+
+// useRef allows to keep array value between renders
   const unfilteredBeersArr = useRef([])
   useEffect(()=>{
     unfilteredBeersArr.current = beersArr;
@@ -16,25 +18,45 @@ const NavMenu = (props) => {
 
   },[])
 
+  // all beers mapped array
   const allBeers = unfilteredBeersArr.current.map((beer) => {
     return beer;
   })
 
+  // Classic beers filterd array
   const filterClassic = unfilteredBeersArr.current.filter((beer) => {
     return beer.first_brewed.slice(3) < 2010;
   })
 
+  // High alcohol filtered array
   const filterABV = unfilteredBeersArr.current.filter((beer) => {
     return beer.abv > 6;
   })
 
+  // High acidity filterd array
   const filterHighAcidity = unfilteredBeersArr.current.filter((beer) => {
     return beer.ph < 4;
     })
 
+    // searchbox input
+    const handleSearchInput = (event) =>{
+      const cleanInput = event.target.value.toLowerCase();
+    
+    const searchBox = unfilteredBeersArr.current.filter((beer) => {
+      return beer.name.toLowerCase().includes(cleanInput);
+      })
+
+      setBeers(searchBox)
+    }
+
+
+
   return (
     <nav className="nav-menu">
-      <p>Search</p>
+      <div>
+        <p>Search Beers</p>
+        <input onInput={handleSearchInput} placeholder="Find Beers" type="text" />
+      </div>
       <p onClick={() =>setBeers(allBeers)}>All Beers</p>
       <p onClick={() =>setBeers(filterClassic)}>Classic Beers</p>
       <p onClick={() =>setBeers(filterABV)}>High ABV</p>
